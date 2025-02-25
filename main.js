7d78e0a94aa9c41c70b459ee2d157bad86526d35
Andressa Ferreira <andressaferre03@gmail.com>
2025-02-25 05:22:11 -0300

    config

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strinc"
+
+// Configuração do dayjs
+import "./hairday-template/src/libs/dayjs.js"
+
+
+// CSS
+import "./hairday-template/src/styles/global.css"
+import "./hairday-template/src/styles/form.css"
+import "./hairday-template/src/styles/schedule.css"
+
